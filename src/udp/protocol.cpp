8ec092c54a154f869425f68857498a59311dd85c
Maarten de Vries <maarten@de-vri.es>
2016-05-16 17:40:39 +0200

    Change UDP protocol to use little endian.

--- src/udp/protocol.cpp
+++ src/udp/protocol.cpp
@@ -8,27 +8,27 @@ namespace udp {
 
 namespace {
 	template<typename T>
-	void writeBigEndian(std::vector<std::uint8_t> & out, T value) {
+	void writeLittleEndian(std::vector<std::uint8_t> & out, T value) {
 		static_assert(std::is_integral<T>::value, "T must be an integral type.");
-		for (unsigned int i = 1; i <= sizeof(T); ++i) {
-			out.push_back(value >> (sizeof(T) - i) * 8 & 0xff);
+		for (unsigned int i = 0; i < sizeof(T); ++i) {
+			out.push_back(value >> i * 8 & 0xff);
 		}
 	}
 
 	template<typename T>
-	T readBigEndian(std::uint8_t const * data) {
+	T readLittleEndian(std::uint8_t const * data) {
 		static_assert(std::is_integral<T>::value, "T must be an integral type.");
 		T result = 0;
 		for (unsigned int i = 0; i < sizeof(T); ++i) {
-			result |= data[i] << (sizeof(T) - i - 1) * 8;
+			result |= data[i] << i * 8;
 		}
 		return result;
 	}
 
 	template<typename T>
-	T readBigEndian(string_view & data) {
+	T readLittleEndian(string_view & data) {
 		data.remove_prefix(sizeof(T));
-		return readBigEndian<T>(reinterpret_cast<std::uint8_t const *>(data.data() - sizeof(T)));
+		return readLittleEndian<T>(reinterpret_cast<std::uint8_t const *>(data.data() - sizeof(T)));
 	}
 
 	void encodeRequestHeader(
@@ -37,14 +37,14 @@ namespace {
 	) {
 		out.reserve(out.size() + header_size + header.payload_size);
 		// Magic bytes.
-		out.push_back('Y'); out.push_back('E'); out.push_back('R'); out.push_back('C');
+		out.insert(out.end(), {'Y', 'E', 'R', 'C'});
 
 		// Header size, payload size.
-		writeBigEndian<std::uint16_t>(out, header_size);
-		writeBigEndian<std::uint16_t>(out, header.payload_size);
+		writeLittleEndian<std::uint16_t>(out, header_size);
+		writeLittleEndian<std::uint16_t>(out, header.payload_size);
 
 		// Reserved magic constant.
-		out.push_back(0x03);
+		out.push_back('3');
 
 		// "Division" (robot command or file command)
 		out.push_back(std::uint8_t(header.division));
@@ -56,14 +56,14 @@ namespace {
 		out.push_back(header.request_id);
 
 		// Block number.
-		writeBigEndian<std::uint32_t>(out, header.block_number);
+		writeLittleEndian<std::uint32_t>(out, header.block_number);
 
 		// Reserved.
 		out.insert(out.end(), 8, '9');
 
 		// Subrequest details
-		writeBigEndian<std::uint16_t>(out, header.command);
-		writeBigEndian<std::uint16_t>(out, header.instance);
+		writeLittleEndian<std::uint16_t>(out, header.command);
+		writeLittleEndian<std::uint16_t>(out, header.instance);
 		out.push_back(header.attribute);
 		out.push_back(header.service);
 
@@ -105,37 +105,37 @@ namespace {
 		data.remove_prefix(4);
 
 		// Check the header size.
-		std::uint16_t parsed_header_size = readBigEndian<std::uint16_t>(data);
+		std::uint16_t parsed_header_size = readLittleEndian<std::uint16_t>(data);
 		if (parsed_header_size != header_size) return malformedResponse("header size (" + std::to_string(parsed_header_size) + ") does not match expected (" + std::to_string(header_size) + ")");
 
 		// Get payload size and make sure the message is complete.
-		result.payload_size = readBigEndian<std::uint16_t>(data);
+		result.payload_size = readLittleEndian<std::uint16_t>(data);
 		if (result.payload_size > max_payaload_size) return malformedResponse("received payload size (" + std::to_string(result.payload_size) + ") exceeds the maximum size (" + std::to_string(max_payaload_size) + ")");
 		if (original.size() != header_size + result.payload_size) return malformedResponse("number of received bytes (" + std::to_string(original.size()) + ") does not match the message size according to the header (" + std::to_string(header_size + result.payload_size) + ")");
 
 		data.remove_prefix(1);
-		result.division = Division(readBigEndian<std::uint8_t>(data));
+		result.division = Division(readLittleEndian<std::uint8_t>(data));
 
 		// Make sure the ack value is correct.
-		std::uint8_t ack = readBigEndian<std::uint8_t>(data);
+		std::uint8_t ack = readLittleEndian<std::uint8_t>(data);
 		if (ack != 1) return malformedResponse("response message ACK value (" + std::to_string(ack) + ") does not match the expected value (1)");
 		result.ack = true;
 
 		// Parse request ID and block number.
-		result.request_id   = readBigEndian<std::uint8_t>(data);
-		result.block_number = readBigEndian<std::uint32_t>(data);
+		result.request_id   = readLittleEndian<std::uint8_t>(data);
+		result.block_number = readLittleEndian<std::uint32_t>(data);
 
 		// Reserved 8 bytes.
 		data.remove_prefix(8);
 
 		// Parse service and status field.
-		result.service = readBigEndian<std::uint8_t>(data);
-		result.status  = readBigEndian<std::uint8_t>(data);
+		result.service = readLittleEndian<std::uint8_t>(data);
+		result.status  = readLittleEndian<std::uint8_t>(data);
 
 		// Parse extra status field. Holy shit.
-		std::uint8_t additional_status_size = readBigEndian<std::uint8_t>(data);
-		data.remove_prefix(2);
-		result.extra_status = readBigEndian<std::uint16_t>(data);
+		std::uint8_t additional_status_size = readLittleEndian<std::uint8_t>(data);
+		data.remove_prefix(1);
+		result.extra_status = readLittleEndian<std::uint16_t>(data);
 
 		// Padding.
 		data.remove_prefix(2);
@@ -147,7 +147,7 @@ namespace {
 
 template<> std::vector<std::uint8_t> encode<ReadByteVariable::Request>(ReadByteVariable::Request const & request, std::uint8_t request_id) {
 	std::vector<std::uint8_t> result;
-	RequestHeader header = makeRobotRequestHeader(0, commands::robot::read_int8_variable, request.index, 1, service::set_single, request_id);
+	RequestHeader header = makeRobotRequestHeader(0, commands::robot::read_int8_variable, request.index, 1, service::get_all, request_id);
 	encodeRequestHeader(result, header);
 	return result;
 }
@@ -156,7 +156,7 @@ template<> ErrorOr<ReadByteVariable::Response> decode<ReadByteVariable::Response
 	ErrorOr<ResponseHeader> header = decodeResponseHeader(message);
 	if (!header.valid()) return header.error();
 	if (header.get().payload_size != 4) return malformedResponse("payload size (" + std::to_string(header.get().payload_size) + ") does not match the expected size (4)");
-	return ReadByteVariable::Response{readBigEndian<std::uint8_t>(message)};
+	return ReadByteVariable::Response{readLittleEndian<std::uint8_t>(message)};
 }
 
 template<> std::vector<std::uint8_t> encode<WriteByteVariable::Request>(WriteByteVariable::Request const & request, std::uint8_t request_id) {
