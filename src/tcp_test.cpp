37a3abe3ddc54f11a4db4bdfeb9187afaf05034f
Maarten de Vries <maarten@de-vri.es>
2016-05-20 14:32:43 +0200

    Meeasure reading speed in test programs.

--- src/tcp_test.cpp
+++ src/tcp_test.cpp
@@ -1,10 +1,22 @@
 #include <yaskawa-ethernet/tcp/client.hpp>
 
 #include <boost/asio/io_service.hpp>
+#include <boost/asio/steady_timer.hpp>
 
 #include <iostream>
+#include <chrono>
 
 dr::yaskawa::tcp::Client * client;
+int read_count = 0;
+boost::asio::steady_timer * timer;
+
+void onTimeout(boost::system::error_code const & error) {
+	if (error) throw boost::system::system_error(error);
+	std::cout << "Reading at " << read_count << " Hz.\n";
+	read_count = 0;
+	timer->expires_from_now(std::chrono::seconds(1));
+	timer->async_wait(onTimeout);
+}
 
 void onReadByte(dr::yaskawa::ErrorOr<std::uint8_t> response) {
 	auto error = response.error();
@@ -13,7 +25,9 @@ void onReadByte(dr::yaskawa::ErrorOr<std::uint8_t> response) {
 		return;
 	}
 
+	++read_count;
 	std::cout << "Read byte variable with value " << int(response.get()) << "\n";
+	client->sendCommand<dr::yaskawa::ReadInt8Variable>({1}, onReadByte);
 }
 
 void onStart(dr::yaskawa::ErrorOr<dr::yaskawa::tcp::CommandResponse> response) {
@@ -25,6 +39,7 @@ void onStart(dr::yaskawa::ErrorOr<dr::yaskawa::tcp::CommandResponse> response) {
 
 	std::cout << "Start request succeeded: " << response.get().message << "\n";
 	client->sendCommand<dr::yaskawa::ReadInt8Variable>({1}, onReadByte);
+	timer->async_wait(onTimeout);
 }
 
 void onConnect(boost::system::error_code const & error) {
@@ -40,6 +55,8 @@ int main(int argc, char * * argv) {
 	boost::asio::io_service ios;
 	dr::yaskawa::tcp::Client client(ios);
 	::client = &client;
+	boost::asio::steady_timer timer(ios);
+	::timer = &timer;
 
 	std::string host = "10.0.0.2";
 	std::string port = "80";
--- src/udp_test.cpp
+++ src/udp_test.cpp
@@ -1,19 +1,31 @@
 #include <yaskawa-ethernet/udp/client.hpp>
 
 #include <boost/asio/io_service.hpp>
+#include <boost/asio/steady_timer.hpp>
 
 #include <iostream>
 
 dr::yaskawa::udp::Client * client;
+int read_count = 0;
+boost::asio::steady_timer * timer;
+
+void onTimeout(boost::system::error_code const & error) {
+	if (error) throw boost::system::system_error(error);
+	std::cout << "Reading at " << read_count << " Hz.\n";
+	read_count = 0;
+	timer->expires_from_now(std::chrono::seconds(1));
+	timer->async_wait(onTimeout);
+}
 
 void onReadByte(dr::yaskawa::ErrorOr<dr::yaskawa::ReadInt8Variable::Response> const & response) {
 	auto error = response.error();
 	if (error) {
 		std::cout << "Error " << error.category().name() << ":" << error.value() << ": " << error.detailed_message() << "\n";
-		return;
+	} else {
+		std::cout << "Read byte variable with value " << int(response.get().value) << "\n";
 	}
-
-	std::cout << "Read byte variable with value " << int(response.get().value) << "\n";
+	++read_count;
+	client->readByteVariable(0, 1500, onReadByte);
 }
 
 void onConnect(boost::system::error_code const & error) {
@@ -22,13 +34,16 @@ void onConnect(boost::system::error_code const & error) {
 		return;
 	}
 	std::cout << "Connected to " << client->socket().remote_endpoint() << " .\n";
-	client->readByteVariable({1}, 1500, onReadByte);
+	client->readByteVariable(0, 1500, onReadByte);
+	timer->async_wait(onTimeout);
 }
 
 int main(int argc, char * * argv) {
 	boost::asio::io_service ios;
 	dr::yaskawa::udp::Client client(ios);
 	::client = &client;
+	boost::asio::steady_timer timer(ios);
+	::timer = &timer;
 
 	std::string host = "10.0.0.2";
 	std::string port = "10040";
