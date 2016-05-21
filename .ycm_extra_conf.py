a4184e7d08507ea9932b2f6c8bbbdaa01fb3f122
Maarten de Vries <maarten@de-vri.es>
2016-05-21 14:25:24 +0200

    Add .ycm_extra_conf.

---
+++ .ycm_extra_conf.py
@@ -0,0 +1,27 @@
+import os
+from glob import glob
+#import ycm_core
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+def FlagsForFile(filename, **kwargs):
+	filedir = os.path.dirname(filename)
+	flags = [
+		'-x', 'c++',
+		'-Wall',
+		'-Wextra',
+		'-Wpedantic',
+		'-fPIC',
+		'-std=c++11',
+		'-isystem', os.path.join(os.environ['HOME'], '.vim/bundle/YouCompleteMe/third_party/ycmd/ycmd/../clang_includes'),
+		'-isystem', '/usr/include/eigen3',
+		'-I', '.',
+	]
+
+	for path in [os.path.join(base, 'include'), os.path.join(base, 'include', 'yaskawa-ethernet')]:
+		flags += ['-I', path]
+
+	return {
+		'flags': flags,
+		'do_cache': True
+	}
