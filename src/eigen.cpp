1955e2a77f8ae01798b0794c1205aa9e56309d76
Maarten de Vries <maarten@de-vri.es>
2016-05-27 19:42:39 +0200

    Fix Eigen orientation conversion.

--- src/eigen.cpp
+++ src/eigen.cpp
@@ -12,9 +12,10 @@ namespace yaskawa {
  */
 Eigen::Isometry3d toEigen(CartesianPosition const & position) {
 	return Eigen::Translation3d{Eigen::Vector3d{position.x(), position.y(), position.z()} * 0.001}
-		* Eigen::AngleAxisd{position.rx() * M_PI / 180.0, Eigen::Vector3d::UnitX()}
+		* Eigen::AngleAxisd{position.rz() * M_PI / 180.0, Eigen::Vector3d::UnitZ()}
 		* Eigen::AngleAxisd{position.ry() * M_PI / 180.0, Eigen::Vector3d::UnitY()}
-		* Eigen::AngleAxisd{position.rz() * M_PI / 180.0, Eigen::Vector3d::UnitZ()};
+		* Eigen::AngleAxisd{position.rx() * M_PI / 180.0, Eigen::Vector3d::UnitX()}
+		;
 }
 
 CartesianPosition toCartesian(Eigen::Isometry3d const & pose, CoordinateSystem system, int tool, CartesianPoseType type) {
@@ -23,13 +24,13 @@ CartesianPosition toCartesian(Eigen::Isometry3d const & pose, CoordinateSystem s
 	result.tool   = tool;
 	result.type   = type;
 
-	Eigen::Vector3d angles = pose.rotation().eulerAngles(0, 1, 2);
+	Eigen::Vector3d angles = pose.rotation().eulerAngles(2, 1, 0);
 	result.x() = pose.translation().x() * 1000;
 	result.y() = pose.translation().y() * 1000;
 	result.z() = pose.translation().z() * 1000;
-	result.rx() = angles[0] / M_PI * 180.0;
+	result.rx() = angles[2] / M_PI * 180.0;
 	result.ry() = angles[1] / M_PI * 180.0;
-	result.rz() = angles[2] / M_PI * 180.0;
+	result.rz() = angles[0] / M_PI * 180.0;
 	return result;
 }
 
