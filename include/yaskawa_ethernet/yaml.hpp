71ac00f99e6194f847b22ac5d7e7736e6ff6ef0c
Evangelos Krikelis <e.krikelis@fizyr.com>
2017-10-12 10:36:46 +0200

    Convert between coordinate systems.

--- include/yaskawa_ethernet/yaml.hpp
+++ include/yaskawa_ethernet/yaml.hpp
@@ -1,24 +1,17 @@
 #include "types.hpp"
-
 #include <yaml-cpp/yaml.h>
-
-#include<string>
-
-namespace dr{
-
-/// Convert Cartesian position to YAML.
-YAML::convert<dr::yaskawa::CartesianPosition>::decode(Node & node, CartesianPosition const & out);
-
-}
+#include <string>
 
 namespace YAML {
-	/// Convert between yamlcpp and CartesianPosition.
 	template<>
 	struct convert<dr::yaskawa::CartesianPosition>{
 		static Node encode(dr::yaskawa::CartesianPosition const & in);
 		static bool decode(Node const & node, dr::yaskawa::CartesianPosition & out);
 	};
 
-
-
+	template<>
+	struct convert<dr::yaskawa::CoordinateSystem>{
+		static Node encode(dr::yaskawa::CoordinateSystem const & in);
+		static bool decode(Node const & node, dr::yaskawa::CoordinateSystem & out);
+	};
 }
--- src/yaml.cpp
+++ src/yaml.cpp
@@ -1,6 +1,4 @@
-
 #include "yaml.hpp"
-
 #include <yaml-cpp/yaml.h>
 
 /// Convert a coordinate system to YAML.
@@ -12,14 +10,15 @@ YAML:: Node YAML::convert<dr::yaskawa::CartesianPosition>::encode(dr::yaskawa::C
 	node["rx"] = in.rx();
 	node["ry"] = in.ry();
 	node["rz"] = in.rz();
-	node["configuration"] = in.configuration();
+	node["configuration"] = std::uint8_t(in.configuration());
 	node["tool"] = in.tool();
 	node["frame"] = in.frame();
 	return node;
 }
 
-bool YAML::convert<dr::yaskawa::CartesianPosition>::decode(Node const & node,dr::yaskawa:: CartesianPosition & out) {
+bool YAML::convert<dr::yaskawa::CartesianPosition>::decode(Node const & node, dr::yaskawa::CartesianPosition & out) {
 	if (node.IsMap() || (node.size() != 9)) return false;
+
 	out = {
 		node["x"].as<double>(),
 		node["y"].as<double>(),
@@ -27,20 +26,20 @@ bool YAML::convert<dr::yaskawa::CartesianPosition>::decode(Node const & node,dr:
 		node["rx"].as<double>(),
 		node["ry"].as<double>(),
 		node["rz"].as<double>(),
-		node["configuration"].as<std::uint8_t>(),
-		node["tool"].as<int>(),
 		node["frame"].as<dr::yaskawa::CoordinateSystem>(),
+		node["configuration"].as<std::uint8_t>(),
+		node["tool"].as<int>()
 	};
 	return true;
 }
 
-YAML::Node YAML::convert<dr::yaskawa::CoordinateSystem>::encode(dr::yaskawa:: CoordinateSystem const & in){
-		dr::yaskawa::CoordinateSystem{node["system"].as
-		return node;
-	}
+YAML::Node YAML::convert<dr::yaskawa::CoordinateSystem>::encode(dr::yaskawa::CoordinateSystem const & in) {
+	YAML::Node node;
+	node["name"] = dr::yaskawa::toString(in);
+	return node;
+}
 
 bool YAML::convert<dr::yaskawa::CoordinateSystem>::decode(Node const & node, dr::yaskawa::CoordinateSystem & out) {
-	out = {
-		node["system"]  = dr::yaskawa::CoordinateSystem.value();
-		return true;
-	}
+	out = dr::yaskawa::toCoordinateSystem(node["name"].as<std::string>()).value();
+	return true;
+}
