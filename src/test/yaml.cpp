4e6b61a0732da48eaf40c9a46c6d11f789b502a2
Evangelos Krikelis <e.krikelis@fizyr.com>
2017-10-12 12:23:48 +0200

    Add example unit test.

--- src/test/yaml.cpp
+++ src/test/yaml.cpp
@@ -1,33 +1,23 @@
-#include "yaml.cpp"
-#include "test/compare.hpp"
-
-#include <yampl-cpp/yaml.h>
-
+#include "yaml.hpp"
 #include <gtest/gtest.h>
 
 using namespace dr;
 
-int main(int arg, char * * argv){
-	testing::InitGoogleTest(&argcm argv);
+int main(int argc, char ** argv){
+	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
 
 namespace dr {
 
-/// Cartesian position test
 TEST(Yaml, toYamlCartesianPosition) {
-	dr::yaskawa::CartesianPosition parsed =  YAML::Load("{x:0.100000, y: 1.200000, z: 2.300000, rx: -1.900000, ry: , rz: , configuration: , tool: , frame: ").as<dr::yaskawa::CoordinateSystem>();
-	ASSERT_TRUE(parsed.isApprox(dr::yaskawa::CartesianPosition{0.1, 1.2, 2.3, -1.9, }));
+	ASSERT_EQ(5, 5);
 }
-
-TEST(Yaml, toYamlCartesianPosition) {
-	ASSERT_EQ("{x: -0.100000, y: 1.200000, z: 2.300000, rx: , ry: , rz: , configuration: , tool: , frame: }", toYaml(dr::yaskawa::CartesianPosition{-0.1, 1.2, 2.3}));
-}
-
 
 TEST(Yaml, toYamlCoordinateSystem) {
-	ASSERT_STRNE("{}",toYaml(dr::yaskawa::CoordinateSystem{}));
+	ASSERT_EQ(0, 0);
 }
+
 }
 
 
